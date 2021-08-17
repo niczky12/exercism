99539d96f30657a35a233ea966935117b1827643
Bence Komarniczky <bence.komarniczky@thetradedesk.com>
2021-08-17 17:44:37 +0100

    better solution

--- julia/word-count/word-count.jl
+++ julia/word-count/word-count.jl
@@ -1,29 +1,11 @@
-const seperators = ('\t', '\n', ' ')
-
 using Pipe
-using DataStructures
-
-function clean_sentence(sentence)
-    @pipe sentence |>
-        lowercase |>
-        # this is ugly and fails 1 test case
-        replace(_, r"[ .\n,\t!\^&*()~@#\$%{}?\[\]:;\/<>]" => " ") |>
-        replace(_, r"\s{2,}" => " ") |>
-        split(_, " ")
-end
+using StatsBase
 
 
 function wordcount(sentence)
-
-    counter = DefaultDict{String, Int64}(0)
-
-    for word in clean_sentence(sentence)
-        word ∈ ("", "'") && continue
-
-        counter[word] += 1
-    end
-
-    return counter
-         
+    @pipe sentence |>
+        lowercase |>
+        replace(_, r"(?!\b'\b)[^\w ]" => " ") |>
+        split(_, " ", keepempty=false) |>
+        countmap
 end
-
