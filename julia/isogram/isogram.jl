ddc859c4fe8f2119101f10122c6d2aa7271772ae
Bence Komarniczky <bence.komarniczky@thetradedesk.com>
2021-08-19 12:27:05 +0100

    add community solution

--- julia/isogram/isogram.jl
+++ julia/isogram/isogram.jl
@@ -1,3 +1,4 @@
+# using StatsBase solution
 using StatsBase
 
 function isisogram(s)
@@ -9,4 +10,24 @@ function isisogram(s)
     values |>
     maximum |>
     x -> x <= 1
+end
+
+# concise one-liner
+isisogram2(s) = s |> lowercase |> word -> filter(isletter, word) |> allunique
+
+
+# crazy fast solution from the community
+using Strs: ASCIIChr
+
+function  isisogram3(str)
+    seen = BitSet()
+    for char in str
+        char = ASCIIChr(char)
+        if isletter(char)
+            letter = Int(lowercase(char))
+            letter in seen && return false
+            push!(seen, letter)
+        end
+    end
+    return true
 end
