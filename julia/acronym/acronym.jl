84de0ff0a4243f1b22aa1cc22617f335d6121668
Bence Komarniczky <bence.komarniczky@thetradedesk.com>
2021-08-23 15:12:05 +0100

    community solution

--- julia/acronym/acronym.jl
+++ julia/acronym/acronym.jl
@@ -3,3 +3,9 @@ function acronym(phrase)
     letters = map(idx -> clean_phrase[idx], findall(r"(?<=\s)(\w)", clean_phrase))
     string(uppercase.(letters)...)
 end
+
+
+# community solution
+function acronym2(phrase)
+    split(phrase, r"[ _-]+").|> first .|> uppercase |> join
+end
