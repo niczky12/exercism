25f93927bea8c7f570c7270975aa04dc29d96d53
Bence Komarniczky <niczky12@gmail.com>
2022-04-05 09:55:22 +0100

    tidy up

--- scala/grade-school/build.sbt
+++ scala/grade-school/build.sbt
@@ -1,4 +1,5 @@
 scalaVersion := "2.13.6"
 
+scalacOptions += "-deprecation"
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.10" % "test"
 
--- scala/grade-school/src/main/scala/GradeSchool.scala
+++ scala/grade-school/src/main/scala/GradeSchool.scala
@@ -1,18 +1,23 @@
 import scala.collection.immutable.SortedMap
 
 class School {
-    type Grade = Seq[String]
-    type DB = Map[Int, Grade]
-    private var _db: DB = Map()
-    private val emptyGrade: Seq[String] = Seq()
+  type Grade = Seq[String]
+  type DB = Map[Int, Grade]
+  private var _db: DB = Map()
+  private val emptyGrade: Grade = Seq()
 
-    def db(): DB = _db
-    def grade(gradeNum: Int): Grade = _db.getOrElse(gradeNum, emptyGrade)
+  def db: DB = _db
+  def grade(gradeNum: Int): Grade = _db.getOrElse(gradeNum, emptyGrade)
 
-    def add(name: String, gradeNum: Int) = {
-        val thisGrade = grade(gradeNum).appended(name)
-        _db = _db + (gradeNum -> thisGrade)
-    }
+  def add(name: String, gradeNum: Int) = {
+    _db = db.updated(gradeNum, grade(gradeNum).appended(name))
+  }
 
-  def sorted: DB = SortedMap.from(_db) map { case (grade, students) => (grade, students.sorted) }
+  def sorted: DB = SortedMap.from(_db) map { case (grade, students) =>
+    (grade, students.sorted)
+  }
+}
+
+object GradeSchool {
+  def main(args: Array[String]) = println("hello world")
 }
